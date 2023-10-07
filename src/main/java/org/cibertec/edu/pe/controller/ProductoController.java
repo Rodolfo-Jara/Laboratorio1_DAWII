07a8daafa89fd2f3ebcdb9d4f5ea79b64d150471
Rodolfo <rodolfojaralopez19@gmail.com>
2023-10-06 19:12:10 -0500

    first commit

--- src/main/java/org/cibertec/edu/pe/controller/ProductoController.java
+++ src/main/java/org/cibertec/edu/pe/controller/ProductoController.java
@@ -5,11 +5,16 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.cibertec.edu.pe.model.Detalle;
 import org.cibertec.edu.pe.model.Producto;
+import org.cibertec.edu.pe.model.Venta;
 import org.cibertec.edu.pe.repository.IDetalleRepository;
 import org.cibertec.edu.pe.repository.IProductoRepository;
 import org.cibertec.edu.pe.repository.IVentaRepository;
@@ -25,7 +30,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@SessionAttributes({"carrito", "total"})
+@SessionAttributes({ "carrito", "total" })
 public class ProductoController {
 	@Autowired
 	private IProductoRepository productoRepository;
@@ -33,7 +38,7 @@ public class ProductoController {
 	private IVentaRepository ventaRepository;
 	@Autowired
 	private IDetalleRepository detalleRepository;
-	
+
 	@GetMapping("/index")
 	public String listado(Model model) {
 		List<Producto> lista = new ArrayList<>();
@@ -41,135 +46,219 @@ public class ProductoController {
 		model.addAttribute("productos", lista);
 		return "index";
 	}
-	//AGREGAR AL CARRITO
+
+	// AGREGAR AL CARRITO
 	@GetMapping("/agregar/{idProducto}")
 	public String agregar(Model model, @PathVariable(name = "idProducto", required = true) int idProducto) {
-		// Obtén el producto que deseas agregar al carrito desde el repositorio
-	    Producto producto = productoRepository.findById(idProducto).orElse(null);
+		Producto producto = productoRepository.findById(idProducto).orElse(null);
 
-	    if (producto != null) {
-	        // Obtén el carrito de la sesión
-	        List<Detalle> carrito = (List<Detalle>) model.getAttribute("carrito");
-
-	        // Verifica si el producto ya está en el carrito
-	        boolean productoEnCarrito = false;
-	        for (Detalle detalle : carrito) {
-	            if (detalle.getIdProducto() == idProducto) {
-	                // Si el producto ya está en el carrito, incrementa la cantidad
-	                detalle.setCantidad(detalle.getCantidad() + 1);
-	                detalle.setSubtotal(detalle.getCantidad() * detalle.getPrecio());
-	                productoEnCarrito = true;
-	                break;
-	            }
-	        }
-
-	        if (!productoEnCarrito) {
-	            // Si el producto no está en el carrito, crea un nuevo detalle
-	            Detalle nuevoDetalle = new Detalle();
-	            nuevoDetalle.setIdProducto(idProducto);
-	            nuevoDetalle.setDescripcion(producto.getDescripcion());
-	            nuevoDetalle.setImagen(producto.getImagen());
-	            nuevoDetalle.setPrecio(producto.getPrecio());
-	            nuevoDetalle.setCantidad(1);
-	            nuevoDetalle.setSubtotal(producto.getPrecio());
-
-	            // Agrega el nuevo detalle al carrito
-	            carrito.add(nuevoDetalle);
-	        }
-
-	        // Actualiza el carrito en la sesión
-	        model.addAttribute("carrito", carrito);
-	    }
-
-	    // Redirige de nuevo a la página de inicio
-	    return "redirect:/index";
+		if (producto != null) {
+			List<Detalle> carrito = (List<Detalle>) model.getAttribute("carrito");
+			boolean productoEnCarrito = false;
+			for (Detalle detalle : carrito) {
+				if (detalle.getIdProducto() == idProducto) {
+					detalle.setCantidad(detalle.getCantidad() + 1);
+					detalle.setSubtotal(detalle.getCantidad() * detalle.getPrecio());
+					productoEnCarrito = true;
+					break;
+				}
+			}
+			if (!productoEnCarrito) {
+				Detalle nuevoDetalle = new Detalle();
+				nuevoDetalle.setIdProducto(idProducto);
+				nuevoDetalle.setDescripcion(producto.getDescripcion());
+				nuevoDetalle.setImagen(producto.getImagen());
+				nuevoDetalle.setPrecio(producto.getPrecio());
+				nuevoDetalle.setCantidad(1);
+				nuevoDetalle.setSubtotal(producto.getPrecio());
+				carrito.add(nuevoDetalle);
+			}
+			model.addAttribute("carrito", carrito);
+		}
+		return "redirect:/index";
 	}
+
 	
-	@GetMapping("/carrito")
-	public String carrito() {
+	// REALIZAR PAGO Y REGISTRAR EN EL DETALLE
+	@GetMapping("/pagar")
+	public String realizarPagoYRegistrar(HttpSession session, Model model) {
+		List<Detalle> cart = (List<Detalle>) session.getAttribute("carrito");
+		double total = (double) session.getAttribute("total");
+		Venta venta = new Venta();
+		venta.setFechaRegistro(new Date());
+		venta.setMontoTotal(total);
+
+		ventaRepository.save(venta);
+		for (Detalle detalle : cart) {
+			detalle.setIdVenta(venta.getIdVenta());
+			detalleRepository.save(detalle);
+		}
+		session.setAttribute("carrito", cart);
+		session.setAttribute("total", total);
+
+		// MENSAJE
+		String mensaje = "PAGO REGISTRADO CON EXITO";
+		model.addAttribute("mensaje", mensaje);
+
 		return "carrito";
 	}
-	@GetMapping("/create")
-	public String create() {
-		return "create";
-	}
-	
-	@GetMapping("/pagar")
-	public String pagar(Model model) {
-	    // Codigo para pagar
-	    return "mensaje";
-	}
 
-	
-	//ACTUALIZAR CARRITO COMPRAS
+	// ACTUALIZAR RL CARRITO
 	@PostMapping("/actualizarCarrito")
-    public String actualizarCarrito(@ModelAttribute("carrito") List<Detalle> carrito, Model model) {
-        // No es necesario obtener el carrito de la sesión, ya que lo estamos pasando como atributo del modelo
+	public String actualizarCarrito(HttpServletRequest request, HttpSession session, Model model) {
+		List<Detalle> carrito = (List<Detalle>) session.getAttribute("carrito");
+		boolean validData = true; 
+		double subtotal = 0.0;
+		double precioEnvio = 0.0;
+		double descuento = 0.0;
 
-        // No necesitas iterar a través de los detalles del carrito aquí, ya que los datos se han vinculado automáticamente
-		System.out.println("Carrito recibido: " + carrito.toString());
-        // Actualiza el carrito en la sesión (esto ya se hace automáticamente debido a la anotación @ModelAttribute)
+		for (Detalle detalle : carrito) {
+			String cantidadParam = request.getParameter("cantidad_" + detalle.getIdProducto());
+			int cantidad = detalle.getCantidad();
 
-        // Redirige de nuevo a la página del carrito
-        return "carrito";
-    }
-	
-	// Inicializacion de variables de la sesion
-	@ModelAttribute("carrito")
-	public List<Detalle> getCarrito() {
-		return new ArrayList<Detalle>();
+			if (cantidadParam != null && !cantidadParam.isEmpty()) {
+				try {
+					cantidad = Integer.parseInt(cantidadParam);
+					detalle.setCantidad(cantidad);
+					detalle.setSubtotal(detalle.getPrecio() * cantidad);
+				} catch (NumberFormatException e) {
+					validData = false; 
+				}
+			}
+
+			subtotal += detalle.getSubtotal();
+			precioEnvio += calcularPrecioEnv(detalle); 
+		}
+
+		String precioEnvioParam = request.getParameter("precioEnvio");
+		String descuentoParam = request.getParameter("descuento");
+
+		
+		try {
+			precioEnvio = Double.parseDouble(precioEnvioParam.replace("S/ ", ""));
+			descuento = Double.parseDouble(descuentoParam.replace("S/ ", ""));
+		} catch (NumberFormatException e) {
+			validData = false;
+		}
+
+		if (validData) {
+			
+			double total = subtotal + precioEnvio - descuento;
+			session.setAttribute("carrito", carrito);
+			session.setAttribute("total", total);
+
+			model.addAttribute("subtotal", subtotal);
+			model.addAttribute("precioEnvio", precioEnvio);
+			model.addAttribute("descuento", descuento);
+			model.addAttribute("total", total);
+		}
+		return "carrito";
 	}
-	
+	private double calcularPrecioEnv(Detalle detalle) {
+		double precioEnvio = 0.0;
+		return precioEnvio;
+	}
 	@ModelAttribute("total")
 	public double getTotal() {
 		return 0.0;
 	}
+	// CREAR PRODUCTO NUEVO
+	@GetMapping("/create")
+	public String create() {
+		return "create";
+	}
 
-	//CREAR PRODUCTO NUEVO
 	@PostMapping("/guardarProducto")
-    public String guardarProducto(@RequestParam("descripcion") String descripcion,
-                                  @RequestParam("precio") double precio,
-                                  @RequestParam("stock") int stock,
-                                  @RequestParam("imagen") MultipartFile imagen) {
+	public String guardarProducto(@RequestParam("descripcion") String descripcion,
+			@RequestParam("precio") double precio, @RequestParam("stock") int stock,
+			@RequestParam("imagen") MultipartFile imagen) {
 
-		// Crear una nueva instancia de Producto y establecer sus propiedades
-	    Producto nuevoProducto = new Producto();
-	    nuevoProducto.setDescripcion(descripcion);
-	    nuevoProducto.setPrecio(precio);
-	    nuevoProducto.setStock(stock);
+		Producto nuevoProducto = new Producto();
+		nuevoProducto.setDescripcion(descripcion);
+		nuevoProducto.setPrecio(precio);
+		nuevoProducto.setStock(stock);
 
-	    try {
-	        // Obtener el contenido de la imagen y guardarla en una ubicación específica
-	        byte[] imagenBytes = imagen.getBytes();
-	     // Generar un nombre único para la imagen utilizando UUID
-	        String nombreArchivo = UUID.randomUUID().toString() + "." + obtenerExtension(imagen.getOriginalFilename());
+		try {
 
-	        String rutaImagen = "src/main/resources/static/img/" + nombreArchivo; // Ruta donde deseas guardar la imagen
+			byte[] imagenBytes = imagen.getBytes();
 
-	        // Guardar la imagen en el sistema de archivos
-	        Path rutaDestino = Paths.get(rutaImagen);
-	        Files.write(rutaDestino, imagenBytes);
+			String nombreArchivo = UUID.randomUUID().toString() + "." + obtenerExtension(imagen.getOriginalFilename());
 
-	        // Establecer la ruta de la imagen en el objeto Producto
-	        nuevoProducto.setImagen(nombreArchivo);
+			String rutaImagen = "src/main/resources/static/img/" + nombreArchivo;
 
-	        // Guardar el producto en la base de datos
-	        productoRepository.save(nuevoProducto);
-	    } catch (IOException e) {
-	        // Manejar errores de carga de archivos
-	        e.printStackTrace();
-	    }
+			Path rutaDestino = Paths.get(rutaImagen);
+			Files.write(rutaDestino, imagenBytes);
 
-	    // Redirigir a la página de inicio u otra página deseada
-	    return "redirect:/index";
-    }
-	//PARA CONVERTIR LA IMAGEN Y GUARDAR EN LA CARPETA IMG
+			nuevoProducto.setImagen(nombreArchivo);
+
+			productoRepository.save(nuevoProducto);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		return "redirect:/index";
+	}
+
+	// PARA CONVERTIR LA IMAGEN Y GUARDAR EN LA CARPETA IMG
 	private String obtenerExtension(String nombreArchivoOriginal) {
-	    String extension = "";
-	    int lastIndex = nombreArchivoOriginal.lastIndexOf(".");
-	    if (lastIndex >= 0) {
-	        extension = nombreArchivoOriginal.substring(lastIndex + 1);
-	    }
-	    return extension;
+		String extension = "";
+		int lastIndex = nombreArchivoOriginal.lastIndexOf(".");
+		if (lastIndex >= 0) {
+			extension = nombreArchivoOriginal.substring(lastIndex + 1);
+		}
+		return extension;
+	}
+	// PARA ELIMINAR UN PRODUCTO DESPUES DE AGREGAR AL CARRITO
+	@GetMapping("/eliminar/{idProducto}")
+	public String eliminar(Model model, @PathVariable(name = "idProducto", required = true) int idProducto,
+			HttpSession session) {
+		List<Detalle> carrito = (List<Detalle>) model.getAttribute("carrito");
+		if (carrito != null) {
+			carrito.removeIf(detalle -> detalle.getIdProducto() == idProducto);
+			session.setAttribute("carrito", carrito);
+
+			// MENSAJE
+			String mensaje = "Producto eliminado correctamente.";
+			model.addAttribute("mensaje", mensaje);
+		}
+		return "redirect:/carrito";
+	}
+	// PARA LISTAR EL EL CARRITO
+	@ModelAttribute("carrito")
+	public List<Detalle> getCarrito() {
+		return new ArrayList<Detalle>();
+	}
+	@GetMapping("/carrito")
+	public String carrito(Model model, HttpSession session) {
+
+		List<Detalle> carrito = (List<Detalle>) session.getAttribute("carrito");
+		double subtotal = calcularSubtotal(carrito);
+		double precioEnvio = calcularPrecioEnvio(carrito);
+		double descuento = calcularDescuento(carrito);
+		double total = subtotal + precioEnvio - descuento;
+
+		model.addAttribute("subtotal", subtotal);
+		model.addAttribute("precioEnvio", precioEnvio);
+		model.addAttribute("descuento", descuento);
+		model.addAttribute("total", total);
+
+		return "carrito";
+	}
+	// METODOS PARA CALCULAR LAS OPERACIONES
+	private double calcularSubtotal(List<Detalle> carrito) {
+		double subtotal = 0.0;
+		for (Detalle detalle : carrito) {
+			subtotal += detalle.getSubtotal();
+		}
+		return subtotal;
+	}
+	//
+	private double calcularPrecioEnvio(List<Detalle> carrito) {
+		return 0.0;
+	}
+
+	private double calcularDescuento(List<Detalle> carrito) {
+		return 0.0;
 	}
 }
